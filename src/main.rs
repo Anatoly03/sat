6da6e6aa2abb9109328e583102c7c9da05a2a748
Anatoly Weinstein <anatoly.wein@gmail.com>
2023-12-29 09:39:16 +0100

    add help message

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,8 @@ use sat::KnfSat;
 use std::{env, str::FromStr};
 
 fn main() {
-    let mut args = env::args().skip(1).into_iter();
+    let mut args = env::args().into_iter();
+    let argc = args.next().unwrap();
     let mut knf_sat: Result<KnfSat, String> = Err("SAT Equation not specified".to_owned());
     let mut version: i8 = 0;
 
@@ -26,9 +27,16 @@ fn main() {
                         version = int;
                         continue;
                     }
-                    panic!("Expected a version number after `-v` but got `{}`", v.as_str())
+                    panic!(
+                        "Expected a version number after `-v` but got `{}`",
+                        v.as_str()
+                    )
                 }
                 panic!("Expected a version number after `-v` but got end of args")
+            }
+            "-h" => {
+                print_help(argc);
+                return;
             }
             _ => {}
         }
@@ -38,4 +46,21 @@ fn main() {
     // TODO with parameter KNF_SAT
 
     dbg!(knf_sat);
+}
+
+fn print_help(argc: String) {
+    println!("======== SAT SOLVER ========
+Usage: {} -- \"SAT\" [OPTIONS]
+Sat Grammar:
+    \x1b[36mS\x1b[0m →\t\x1b[36mA\x1b[0m \x1b[33m| \t\x1b[36mS\x1b[0m \x1b[31m\"|\" \x1b[36mS\x1b[0m\t\t\x1b[30m% Example Sat: 1 2 -3 | 3 4 -5 | -5 -6\x1b[0m
+    \x1b[36mA\x1b[0m →\t\x1b[36mN\x1b[0m \x1b[33m| \t\x1b[36mA\x1b[0m \x1b[31m\"|\" \x1b[36mA\x1b[0m
+    \x1b[36mN\x1b[0m →\t\x1b[31m\"-\"\x1b[0m \x1b[33m? \x1b[36mI\x1b[0m   \t\t\x1b[30m% Negativity represents Negation
+    \x1b[36mI\x1b[0m →\t\x1b[33m[\x1b[31m1-9\x1b[33m] [\x1b[31m0-9\x1b[33m]*\t\t\x1b[30m% Integers represent variable identifiers\x1b[0m
+Options: 
+    -v : Run specific algorithm
+        0 : Brute Force
+    -h : Print this help message
+============================",
+        argc
+    );
 }
