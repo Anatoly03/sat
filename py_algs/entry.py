f4fecd199373913ff1cf2d0b64d7927a32c67cff
Anatoly Weinstein <anatoly.wein@gmail.com>
2023-12-30 12:07:11 +0100

    fix python's entry

--- py_algs/entry.py
+++ py_algs/entry.py
@@ -1,3 +1,4 @@
+import math
 import sys
 import subprocess
 
@@ -6,7 +7,30 @@ algs = [
 ]
 
 def eq_to_list(eq):
-    return []
+    variables = []
+    out = [[]]
+
+    for clause in eq.split('|'):
+        for var in clause.split():
+            variable, new = int(var), 0
+            abs_var = abs(variable)
+
+            if abs_var in variables:
+                new = variables.index(abs_var) + 1
+            else:
+                variables.append(abs_var)
+                new = len(variables)
+
+            out[-1].append(int(math.copysign(new, variable)))
+
+        out.append([])
+
+    del out[-1]
+    
+    return out
+
+def varcount(eq_list):
+    return max([abs(v) for clauses in eq_list for v in clauses])
 
 def main():
     alg = 'bf0'
@@ -17,14 +41,19 @@ def main():
         match arg:
             case '-v':
                 alg = sys.argv[idx + 1]
-
+                # TODO sanitize
                 break
             case '--':
                 eq = sys.argv[idx + 1]
+                # TODO sanitize
                 break
             case '-h':
                 print('TODO Help Message') # TODO
                 sys.exit(0)
+
+    ls = eq_to_list(eq)
+
+    print(f'> {alg} -- Equation(C{len(ls)}, V{varcount(ls)})')
             
     subprocess.run(['python3', alg + '.py', eq])
 
