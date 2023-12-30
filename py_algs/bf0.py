866223b0b7337003da8ddac259aae7e64ef9ad12
Anatoly Weinstein <anatoly.wein@gmail.com>
2023-12-30 12:24:51 +0100

    impl brute force

--- py_algs/bf0.py
+++ py_algs/bf0.py
@@ -1,5 +1,29 @@
+import math
+import itertools
 import sys
-import entry
-eq = entry.eq_to_list(sys.argv[1:][0])
 
-print('Running bf0... with ' + str(eq))
+from entry import eq_to_list, varcount
+EQ = eq_to_list(sys.argv[1:][0])
+
+SOLUTIONS = 0
+
+def solve(EQC, verify, mode):
+    if isinstance(EQC, int):
+        # print(f"{EQC} * {verify} = {EQC * verify[abs(EQC) - 1] > 0}")
+        return EQC * verify[abs(EQC) - 1] > 0
+    
+    return mode([solve(clause, verify, any) for clause in EQC])
+
+    # for clause in EQC:
+    #     if not solve(clause, verify):
+    #         return False
+
+    # return True
+
+for verify in itertools.product((-1, +1), repeat=varcount(EQ)):
+    if solve(EQ.copy(), verify, all):
+        if SOLUTIONS == 0:
+            print(f'Solution: {verify}')
+        SOLUTIONS += 1
+
+print(f'The equation has {SOLUTIONS} solutions.')
