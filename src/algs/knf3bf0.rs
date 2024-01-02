0b283aef6d9b4230686f331a02416aad54b666c4
Anatoly Weinstein <anatoly.wein@gmail.com>
2024-01-02 11:51:23 +0100

    add algorithm template

---
+++ src/algs/knf3bf0.rs
@@ -0,0 +1,33 @@
+use crate::sat::{SatSolver, Variable, KnfSat};
+
+/**
+ * Algorithm: 3-KNF-SAT Brute Force
+ * Complexity: Exponential
+ */
+pub struct Sat3KnfBruteForce {
+    sat: KnfSat
+}
+
+impl SatSolver for Sat3KnfBruteForce {
+    fn new(sat: KnfSat) -> Self {
+        Sat3KnfBruteForce {
+            sat
+        }
+    }
+
+    fn optimise(&mut self) {
+    
+    }
+
+    fn solve(&self) -> Option<Vec<Variable>> {
+        let variables = self.sat.varsize();
+
+        
+
+        None
+    }
+
+    fn solve_all(&self) -> Vec<Vec<Variable>> {
+        todo!()
+    }
+}
