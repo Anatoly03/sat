56734e67b21f5ac44373c4958f93d5840fee3b0c
Anatoly Weinstein <anatoly.wein@gmail.com>
2024-01-02 11:51:30 +0100

    amend

--- src/algs/mod.rs
+++ src/algs/mod.rs
@@ -1,9 +1,11 @@
 use crate::sat::{SatSolver, KnfSat};
 mod bf0;
+mod knf3bf0;
 
-pub fn get_algorithm<'a>(sat: KnfSat, alg: u8) -> impl SatSolver {
+pub fn get_algorithm<T: SatSolver>(sat: KnfSat, alg: u8) -> T {
     match alg {
-        0 => bf0::BruteForce::new(sat),
+        0 => return bf0::BruteForce::new(sat),
+        // 1 => return knf3bf0::Sat3KnfBruteForce::new(sat),
         _ => panic!("Version {} does not exist. Use `-h` option to see which do.", alg)
     }
 }
